60f061fda38d4e74cbaf002ed278ada7d33a4174
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-11-01 17:47:01 +0700

    push

---
+++ POST/function.go
@@ -0,0 +1,30 @@
+package cloudfunction_wegotour
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	tour "github.com/wegotour/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("WegotourPost", WegotourPost)
+}
+
+func WegotourPost(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://wegotour.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://wegotour.github.io")
+	fmt.Fprintf(w, tour.GCFPostHandler("PASETOPRIVATEKEY", "MONGOSTRING", "wegotour", "user", r))
+
+}
