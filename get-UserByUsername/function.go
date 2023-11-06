3dff27ac511d61bb04481fbba2eb71bba4b72ae5
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-11-06 20:18:51 +0700

    Add Cloud Function to get-UserByUsername

---
+++ get-UserByUsername/function.go
@@ -0,0 +1,19 @@
+package cloudfunction_wegotour
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	ticket "github.com/wegotour/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("WegotourGetUser", WegotourGetUser)
+}
+
+func WegotourGetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "https://wegotour.github.io")
+	fmt.Fprintf(w, ticket.GCFHandlerGetUser("MONGOSTRING", "wegotour", "user", "username"))
+}
