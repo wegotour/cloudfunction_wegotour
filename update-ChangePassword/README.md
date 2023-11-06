8c4d39763d9eada3252928f94ae4479485e5c4ff
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-11-06 21:05:39 +0700

    Add Cloud Function for Change Password

---
+++ update-ChangePassword/README.md
@@ -0,0 +1,1 @@
+# Cloud Function Change Password User
---
+++ update-ChangePassword/function.go
@@ -0,0 +1,29 @@
+package cloudfunction_wegotour
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+
+	ticket "github.com/wegotour/be_p3/modul"
+)
+
+func init() {
+	functions.HTTP("WegotourChangePassword", WegotourChangePass)
+}
+
+func WegotourChangePass(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://wegotour.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://wegotour.github.io")
+	fmt.Fprintf(w, ticket.GCFHandlerChangePassword("PASETOPRIVATEKEY", "MONGOSTRING", "wegotour", "user", r))
+}
